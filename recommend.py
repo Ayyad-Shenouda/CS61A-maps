31dd9820d52461732be2f212dee4f56dbf44da2f
Mutjas <ayyaadshenoda15@gmail.com>
2016-04-10 13:40:16 +0200

    project complete

---
+++ recommend.py
@@ -0,0 +1,230 @@
+"""A Yelp-powered Restaurant Recommendation Program"""
+
+from abstractions import *
+from data import ALL_RESTAURANTS, CATEGORIES, USER_FILES, load_user_file
+from ucb import main, trace, interact
+from utils import distance, mean, zip, enumerate, sample
+from visualize import draw_map
+
+################################## 
+# Phase 2: Unsupervised Learning #
+##################################
+
+
+def find_closest(location, centroids):
+    """Return the centroid in centroids that is closest to location. If
+    multiple centroids are equally close, return the first one.
+
+    >>> find_closest([3.0, 4.0], [[0.0, 0.0], [2.0, 3.0], [4.0, 3.0], [5.0, 5.0]])
+    [2.0, 3.0]
+    """
+    min = distance(location, centroids[0])
+    clotest = centroids[0]
+    for center in centroids:
+        dist = distance(location, center)
+        if dist < min :
+            min = dist
+            clotest = center
+    return clotest
+
+
+
+def group_by_first(pairs):
+    """Return a list of pairs that relates each unique key in the [key, value]
+    pairs to a list of all values that appear paired with that key.
+
+    Arguments:
+    pairs -- a sequence of pairs
+
+    >>> example = [ [1, 2], [3, 2], [2, 4], [1, 3], [3, 1], [1, 2] ]
+    >>> group_by_first(example)
+    [[2, 3, 2], [2, 1], [4]]
+    """
+    keys = []
+    for key, _ in pairs:
+        if key not in keys:
+            keys.append(key)
+    return [[y for x, y in pairs if x == key] for key in keys]
+
+
+def group_by_centroid(restaurants, centroids):
+    """Return a list of clusters, where each cluster contains all restaurants
+    nearest to a corresponding centroid in centroids. Each item in
+    restaurants should appear once in the result, along with the other
+    restaurants closest to the same centroid.
+    """
+    clusters=[]
+    for restaurant in restaurants:
+        current_restaurant_location = restaurant_location(restaurant)
+        cluster = find_closest(current_restaurant_location,centroids)
+        clusters.append([cluster, restaurant])
+    return group_by_first(clusters)
+
+
+
+
+def find_centroid(cluster):
+    """Return the centroid of the locations of the restaurants in cluster."""
+    centor_latitude = [restaurant_location(restaurant)[0] for restaurant in cluster]
+    centor_longitude = [restaurant_location(restaurant)[1] for restaurant in cluster]
+    return [mean(centor_latitude),mean(centor_longitude)]
+
+
+def k_means(restaurants, k, max_updates=100):
+    """Use k-means to group restaurants by location into k clusters."""
+    assert len(restaurants) >= k, 'Not enough restaurants to cluster'
+    old_centroids, n = [], 0
+    # Select initial centroids randomly by choosing k different restaurants
+    centroids = [restaurant_location(r) for r in sample(restaurants, k)]
+
+    while old_centroids != centroids and n < max_updates:
+        old_centroids = centroids
+        clusters = group_by_centroid(restaurants, old_centroids)
+        centroids = [find_centroid(cluster) for cluster in clusters]
+        n += 1
+    return centroids
+
+from math import sqrt
+
+def find_predictor(user, restaurants, feature_fn):
+    """Return a rating predictor (a function from restaurants to ratings),
+    for a user by performing least-squares linear regression using feature_fn
+    on the items in restaurants. Also, return the R^2 value of this model.
+
+    Arguments:
+    user -- A user
+    restaurants -- A sequence of restaurants
+    feature_fn -- A function that takes a restaurant and returns a number
+    """
+    reviews_by_user = {review_restaurant_name(review): review_rating(review)
+                       for review in user_reviews(user).values()}
+
+    xs = [feature_fn(r) for r in restaurants]
+    ys = [reviews_by_user[restaurant_name(r)] for r in restaurants]
+    mean_xs = mean(xs)
+    mean_ys = mean(ys)
+    Sxx = sum([(x - mean_xs) ** 2 for x in xs])
+    Syy = sum([(y - mean_ys) ** 2 for y in ys])
+    Sxy = sum([(x - mean_xs) * (y - mean_ys) for x, y in zip(xs, ys)])
+
+    b = Sxy / Sxx 
+    a = mean_ys - b * mean_xs
+    r_squared = Sxy ** 2 / (Sxx * Syy)
+
+    def predictor(restaurant):
+        return b * feature_fn(restaurant) + a
+
+    return predictor, r_squared
+
+
+def best_predictor(user, restaurants, feature_fns):
+    """Find the feature within feature_fns that gives the highest R^2 value
+    for predicting ratings by the user; return a predictor using that feature.
+
+    Arguments:
+    user -- A user
+    restaurants -- A list of restaurants
+    feature_fns -- A sequence of functions that each takes a restaurant
+    """
+    reviewed = user_reviewed_restaurants(user, restaurants)
+    predictor = [list(find_predictor(user, reviewed, fn)) for fn in feature_fns]
+
+    predictor = max(predictor, key = lambda x : x[1])
+    return predictor[0]
+
+
+def rate_all(user, restaurants, feature_fns):
+    """Return the predicted ratings of restaurants by user using the best
+    predictor based a function from feature_fns.
+
+    Arguments:
+    user -- A user
+    restaurants -- A list of restaurants
+    feature_fns -- A sequence of feature functions
+    """
+    predictor = best_predictor(user, ALL_RESTAURANTS, feature_fns)
+    reviewed = user_reviewed_restaurants(user, restaurants)
+    ratting = {}
+    for restaurant in restaurants:
+        restaurantName = restaurant_name(restaurant)
+        if restaurant in reviewed:
+            ratting[restaurantName] = user_rating(user, restaurantName)
+        else :
+            ratting[restaurantName] = predictor(restaurant)
+    
+    return ratting
+
+
+def search(query, restaurants):
+    """Return each restaurant in restaurants that has query as a category.
+
+    Arguments:
+    query -- A string
+    restaurants -- A sequence of restaurants
+    """
+    return [restaurant for restaurant in restaurants if query in restaurant_categories(restaurant)]
+
+
+def feature_set():
+    """Return a sequence of feature functions."""
+    return [restaurant_mean_rating,
+            restaurant_price,
+            restaurant_num_ratings,
+            lambda r: restaurant_location(r)[0],
+            lambda r: restaurant_location(r)[1]]
+
+
+@main
+def main(*args):
+    import argparse
+    parser = argparse.ArgumentParser(
+        description='Run Recommendations',
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument('-u', '--user', type=str, choices=USER_FILES,
+                        default='test_user',
+                        metavar='USER',
+                        help='user file, e.g.\n' +
+                        '{{{}}}'.format(','.join(sample(USER_FILES, 3))))
+    parser.add_argument('-k', '--k', type=int, help='for k-means')
+    parser.add_argument('-q', '--query', choices=CATEGORIES,
+                        metavar='QUERY',
+                        help='search for restaurants by category e.g.\n'
+                        '{{{}}}'.format(','.join(sample(CATEGORIES, 3))))
+    parser.add_argument('-p', '--predict', action='store_true',
+                        help='predict ratings for all restaurants')
+    parser.add_argument('-r', '--restaurants', action='store_true',
+                        help='outputs a list of restaurant names')
+    args = parser.parse_args()
+
+    # Output a list of restaurant names
+    if args.restaurants:
+        print('Restaurant names:')
+        for restaurant in sorted(ALL_RESTAURANTS, key=restaurant_name):
+            print(repr(restaurant_name(restaurant)))
+        exit(0)
+
+    # Select restaurants using a category query
+    if args.query:
+        restaurants = search(args.query, ALL_RESTAURANTS)
+    else:
+        restaurants = ALL_RESTAURANTS
+
+    # Load a user
+    assert args.user, 'A --user is required to draw a map'
+    user = load_user_file('{}.dat'.format(args.user))
+
+    # Collect ratings
+    if args.predict:
+        ratings = rate_all(user, restaurants, feature_set())
+    else:
+        restaurants = user_reviewed_restaurants(user, restaurants)
+        names = [restaurant_name(r) for r in restaurants]
+        ratings = {name: user_rating(user, name) for name in names}
+
+    # Draw the visualization
+    if args.k:
+        centroids = k_means(restaurants, min(args.k, len(restaurants)))
+    else:
+        centroids = [restaurant_location(r) for r in restaurants]
+    draw_map(centroids, restaurants, ratings)
